8b234730c3982caafcb9bf11905a9fbaaa7a8c64
remi <remi.delgutte@apprenant.ifapme>
2023-12-02 22:41:03 +0100

    ajout menu burger, modif dark mode

--- script/script.js
+++ script/script.js
@@ -5,6 +5,11 @@ const isDarkMode = localStorage.getItem("darkMode") === "enabled";
 function toggleDarkMode() {
   const body = document.body;
   const isDarkModeEnabled = body.classList.toggle("dark-mode");
+  if (isDarkModeEnabled) {
+    changeClassOn();
+  } else {
+    changeClassOff();
+  }
 
   // Store the user's preference in local storage
   localStorage.setItem("darkMode", isDarkModeEnabled ? "enabled" : "disabled");
@@ -13,9 +18,27 @@ function toggleDarkMode() {
 // Set initial dark mode based on user preference
 if (isDarkMode) {
   document.body.classList.add("dark-mode");
+  changeClassOn();
+} else {
+  changeClassOff();
 }
 
 // Attach event listener to the button for toggling dark mode
 document
   .getElementById("DarkModeToggle")
   .addEventListener("click", toggleDarkMode);
+
+const nav = document.querySelector(".nav");
+
+const menu = document.querySelector("#icons");
+menu.addEventListener("click", function (e) {
+  nav.classList.toggle("active");
+});
+function changeClassOn() {
+  document.getElementById("img-darkmode").className =
+    "fa-solid fa-toggle-on fa-xl";
+}
+function changeClassOff() {
+  document.getElementById("img-darkmode").className =
+    "fa-solid fa-toggle-off fa-xl";
+}
